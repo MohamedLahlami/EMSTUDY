5e5ef424a3322cf2f18ad7650092e8d2de765aa2
MohamedLahlami <mohamedlahlami@gmail.com>
2025-04-18 01:01:08 +0100

    added auth controller

---
+++ Backend/EMSTUDY/src/main/java/ma/emsi/emstudy/Controller/AuthController.java
@@ -0,0 +1,16 @@
+package ma.emsi.emstudy.Controller;
+
+
+import lombok.RequiredArgsConstructor;
+import ma.emsi.emstudy.Entity.User;
+import ma.emsi.emstudy.Repository.UserRepo;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class AuthController {
+
+}
--- Backend/EMSTUDY/src/main/java/ma/emsi/emstudy/Security/SecurityConfig.java
+++ Backend/EMSTUDY/src/main/java/ma/emsi/emstudy/Security/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -20,11 +21,11 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable())
-                .cors(cors -> cors.disable())
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .formLogin(AbstractHttpConfigurer::disable)
                 .securityMatcher("/**")
-                .formLogin(formLogin -> formLogin.disable())
                 .authorizeHttpRequests(registry -> registry
                         .requestMatchers("/").permitAll()
                         .anyRequest().authenticated()
