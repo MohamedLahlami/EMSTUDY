72181f38ebd602ce2d10fc384243ebcf39e9e865
MohamedLahlami <mohamedlahlami@gmail.com>
2025-05-26 18:53:04 +0100

    fixed CORS issue in docker

--- Backend/EMSTUDY/src/main/java/ma/emsi/emstudy/Security/SecurityConfig.java
+++ Backend/EMSTUDY/src/main/java/ma/emsi/emstudy/Security/SecurityConfig.java
@@ -89,7 +89,7 @@ public class SecurityConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOriginPatterns("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
